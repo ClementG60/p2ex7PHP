25469acd9cf2116bfd3a6da84a9c3e0444b14c3d
ClementG60 <cle_guillaume@hotmail.fr>
2021-12-07 14:10:48 +0100

    ajout code

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+        $isOk = false;
+        if ($isOk == false) {
+            echo 'c\'est pas bon !!!';
+        } else {
+            echo 'c\est ok !!';
+        }
+        ?>
+    </p>
+</body>
+</html>
